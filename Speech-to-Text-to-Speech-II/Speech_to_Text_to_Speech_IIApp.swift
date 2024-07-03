945a4ea627208d7ffad1fd02b19a57a78532de99
James Alan Bush <jamesalanbush1972@gmail.com>
2024-07-03 18:24:43 -0400

    Initial Commit

---
+++ Speech-to-Text-to-Speech-II/Speech_to_Text_to_Speech_IIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speech_to_Text_to_Speech_IIApp.swift
+//  Speech-to-Text-to-Speech-II
+//
+//  Created by Xcode Developer on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Speech_to_Text_to_Speech_IIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
